844af0e3fab0191771b5d531459ab142cf2b175b
Viktor Rura <ruravictor@gmail.com>
2024-03-02 16:16:04 +0000

    Implement viewsets for theatre app models and configure routers.

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import GenreViewSet
+from .views import (
+    GenreViewSet,
+    ActorViewSet,
+    TheatreHallViewSet,
+    PlayViewSet,
+    PerformanceViewSet,
+    ReservationViewSet,
+)
 
 
 router = routers.DefaultRouter()
 router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("theatre_hall", TheatreHallViewSet)
+router.register("play", PlayViewSet)
+router.register("performance", PerformanceViewSet)
+router.register("reservation", ReservationViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
--- theatre/views.py
+++ theatre/views.py
@@ -1,16 +1,47 @@
 from rest_framework import mixins
-from .serializers import GenreSerializer
 from rest_framework.viewsets import GenericViewSet
-
-from .models import (
-    Genre,
+from .models import Genre, Actor, TheatreHall, Play, Performance, Reservation
+from .serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    TheatreHallSerializer,
+    PlaySerializer,
+    PerformanceSerializer,
+    ReservationSerializer,
 )
 
 
-class GenreViewSet(
-    GenericViewSet,
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-):
+class GenreViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+
+
+class ActorViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class TheatreHallViewSet(
+    GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin
+):
+    queryset = TheatreHall.objects.all()
+    serializer_class = TheatreHallSerializer
+
+
+class PlayViewSet(GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin):
+    queryset = Play.objects.all()
+    serializer_class = PlaySerializer
+
+
+class PerformanceViewSet(
+    GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin
+):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+
+
+class ReservationViewSet(
+    GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin
+):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
