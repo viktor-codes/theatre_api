438f8bfae18d2b4027aa5f7095331ce0b6c177a2
Viktor Rura <ruravictor@gmail.com>
2024-03-03 15:07:31 +0000

    Update PerformanceViewSet for retrieving performances with ticket availability.

--- theatre/views.py
+++ theatre/views.py
@@ -1,4 +1,7 @@
-from rest_framework import mixins
+from datetime import datetime
+
+from django.db.models import Count, F
+from rest_framework import mixins, viewsets
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import GenericViewSet
 from .models import Genre, Actor, TheatreHall, Play, Performance, Reservation
@@ -53,14 +56,33 @@ class PlayViewSet(
         return PlaySerializer
 
 
-class PerformanceViewSet(
-    GenericViewSet,
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-):
-    queryset = Performance.objects.all()
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Performance.objects.all()
+        .select_related("play", "theatre_hall")
+        .annotate(
+            tickets_available=F("theatre_hall__rows") * F("theatre_hall__seats_in_row")
+            - Count("tickets")
+            - 1
+        )
+    )
+
     serializer_class = PerformanceSerializer
+
+    def get_queryset(self):
+        date = self.request.query_params.get("date")
+        play_id_str = self.request.query_params.get("play")
+
+        queryset = self.queryset
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(show_time__date=date)
+
+        if play_id_str:
+            queryset = queryset.filter(play_id=int(play_id_str))
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
