6513f738566dfdb43b65c299799679b3a01c6f44
Viktor Rura <ruravictor@gmail.com>
2024-03-03 15:06:04 +0000

    Add related name to ticket fields.

--- theatre/models.py
+++ theatre/models.py
@@ -75,8 +75,12 @@ class Reservation(models.Model):
 class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
-    performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
-    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    performance = models.ForeignKey(
+        Performance, on_delete=models.CASCADE, related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation, on_delete=models.CASCADE, related_name="tickets"
+    )
 
     @staticmethod
     def validate_ticket(
