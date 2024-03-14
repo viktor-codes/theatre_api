529f5a3b4c48946116dee631b7cfb95281f93c45
Viktor Rura <ruravictor@gmail.com>
2024-03-14 15:54:25 +0000

    Run makemigrations to remove username from User instance.

--- theatre_api/settings.py
+++ theatre_api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
